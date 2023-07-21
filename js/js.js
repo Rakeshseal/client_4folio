dcc52090c4300640701d26e29c6b8f5d5b4c289b
Rakesh Seal <rakeshseal48@gmail.com>
2023-07-21 20:02:53 +0530

    done

---
+++ js/js.js
@@ -0,0 +1,16 @@
+// let moreBtn = document.querySelector(".more_btn");
+// let arrowUP = document.querySelector(".arrow_up");
+// let arrowDown = document.querySelector(".arrow_down");
+// let galleryRow = document.querySelector(".gallery_display");
+
+// function arrow_dow(){
+//   galleryRow.style.display="block";
+  
+  
+// }
+// function dblClick(){
+//   galleryRow.style.display="none";
+  
+// }
+
+
